2275b6d913cd7eeaaced3ab9849a1cd6b7a983b3
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2022-03-04 10:37:34 -0300

    Adicionando o exemplo idade

---
+++ Aula 03 - Introdução a Linguagem C/Idade.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){ //ponto de partida do meu programa
+	
+int idade; // declaração da variável
+
+printf("Digite a sua idade: "); // interação com o usuário
+
+scanf("%d",&idade); // scanf faz a leitura da idade do usuário que foi digitada via teclado e armazena essa informação na variável idade	
+	
+printf("Voce tem %d anos",idade);	
+	
+	
+}
