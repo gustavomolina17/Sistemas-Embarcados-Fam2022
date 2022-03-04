1de831078f98a737b693a3faa3b859a5934de704
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2022-03-04 10:35:45 -0300

    Adicionando Exemplo 01

---
+++ Aula 03 - Introdução a Linguagem C/Exemplo1.c
@@ -0,0 +1,10 @@
+#include <stdio.h> //Diretiva para incluir uma biblioteca pré-compilada
+
+int main (void) // Declaração da função main
+{
+
+printf("Esse eh o meu primeiro codigo em C"); // printf -> para exibir uma mensagem em tela
+
+return 0;	
+	
+}
